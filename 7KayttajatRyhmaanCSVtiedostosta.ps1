c91a80a33c6654344cc2d8eddd0ddf1a2e9b1244
JuhaRau <142009002+JuhaRau@users.noreply.github.com>
2024-10-09 14:36:50 +0300

    Add files via upload

---
+++ 7KayttajatRyhmaanCSVtiedostosta.ps1
@@ -0,0 +1,17 @@
+﻿# LISÄÄ KÄYTTÄJÄT RYHMÄÄN CSV-TIEDOSTOSTA
+# =======================================
+
+# Tiedoston nimi ja polku
+$Tiedosto = "C:\Users\Administrator\Documents\Johtoryhmanjasenet.csv"
+
+# Ryhmän nimi
+$Ryhmä = "Johto"
+
+# Lista ryhmän jäsenistä
+$Lista = Import-Csv -Path $Tiedosto -Delimiter ";" -Encoding Default
+
+# Putkitetaan lista ryhmän jäseniksi käyttämällä foreach-silmukkaa
+foreach($Rivi in $Lista)
+{
+    Add-ADGroupMember -Identity $Ryhmä -Members $Rivi.SamAccountName
+}
---
+++ 7PowerShellISESkritp lisätään uusia AD-käyttäjiä csv-tiedostosta.ps1
@@ -0,0 +1,13 @@
+﻿# LISÄTÄÄN UUSIA AD-KÄYTTÄJIÄ CSV-TIEDOSTOSTA
+
+#Tiedoston polku, josta käyttäjätiedot löytyvät
+$Tiedosto = "C:\Users\Administrator\Documents\NewUsers.csv"
+
+# Muodostetaan lista lisättävistä käyttäjistä lukemalla tiedosto
+$Käyttäjälista = Import-Csv -Delimiter ";" -Path $Tiedosto -Encoding Default
+
+#Kryptataan ensimmäinen salasana
+$Salasana = ConvertTo-SecureString -String "Q2werty7" -AsPlainText -Force
+
+# Putkitetaan lista käyttäjien luontikomennolle
+$Käyttäjälista | New-ADUser -AccountPassword $Salasana -ChangePasswordAtLogon 1 -Enabled 1
